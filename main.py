0a25a5c55df612b086ab38a0b19c9552982b0715
AleksandraStecka <254584@student.pwr.edu.pl>
2022-06-03 23:34:33 +0200

    svm and parameter tuning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from data_reader import process_and_import_into_csv
 from data_reader import read_from_csv
 from data_analyser import dataset_analysis
-from classifiers import *
+from classifiers import multinomial_naive_bayes, support_vector_machine, tuning
 import pandas
 
 
@@ -10,4 +10,6 @@ if __name__ == '__main__':
     # dataset_to_analyse = read_from_csv()
     # dataset_analysis(dataset_to_analyse)
     # dataset = pandas.read_csv('booksummaries/processed_data.csv', names=['id', 'genre', 'summary'])
-    multinomial_naive_bayes()
+    multinomial_naive_bayes(0.75, 0.5, 0.001, 5000, (1, 3), 0.1)
+    support_vector_machine(0.75, 0.5, 0.001, 5000, (1, 3), 0.1, 'squared_hinge')
+    # tuning()
